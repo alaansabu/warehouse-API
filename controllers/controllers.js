40c3d59ee98d189bfa88922b4fe335eec324b4ee
Alan Sabu~ <alansabu6046@gmail.com>
2026-01-27 21:49:59 +0530

    stock functions PUT and DELETE completed

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -80,7 +80,61 @@ res.status(200).json({"message":"successfully updated resoources"})
 }
 
 
+const putStock = async (req, res) => {
+    try {
+        const { _id } = req.params;
+        if (!_id) {
+            return res.status(400).json({ "message": "Product ID is required" });
+        }
+
+        // Strict PUT: Replace the whole document with req.body
+        const putdata = await stockSchema.findOneAndReplace(
+            { _id: _id },
+            req.body,
+            { new: true } // This returns the document AFTER replacement
+        );
+
+        if (!putdata) {
+            // Use 'res', not 'resizeBy'! 
+            // Also using the dynamic language property we'll set up next
+            return res.status(404).json({ "message": req.lang?.NOT_FOUND || "Not Found" });
+        }
+
+        return res.status(200).json({
+            "message": req.lang?.success || "Successfully replaced",
+            "data": putdata
+        });
+
+    } catch (error) {
+        console.error("Internal Error:", error);
+        return res.status(500).json({ "message": "Internal Server Error" });
+    }
+};
+
+const delstock = async (req,res)=>{
+
+
+    try {
+
+
+    const { _id } = req.params;
+
+            if (!_id) {
+                return res.status(400).json({ "message": "Product ID is required" });
+            }
+
+            // silent del: delete the whole document with req.body
+            const putdata = await stockSchema.findOneAndDelete(_id)
+            res.status(200).json({"message":"successfully deleted"})
+    } catch (error) {
+        
+        console.log(error);
+        
+    }
 
 
 
-module.exports = {addStock,upadateStock}
+}
+
+
+module.exports = {addStock,upadateStock,putStock,delstock}
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {addStock,upadateStock} = require('../controllers/controllers') 
+const {addStock,upadateStock,putStock,delstock} = require('../controllers/controllers') 
 
 const Router = express.Router()
 
 Router.post("/addStock",addStock)
 Router.patch("/updateDetails/:brand",upadateStock)
-
+Router.put('/putStock/:_id',putStock)
+Router.delete('/del/:_id',delstock)
 
 module.exports = Router
