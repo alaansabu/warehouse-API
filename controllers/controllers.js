a73db6a5d529adce79dfbdde69e44d8b08ef2781
Alan Sabu~ <alansabu6046@gmail.com>
2026-02-01 02:03:26 +0530

    catching del funtion error

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -41,12 +41,12 @@ await stockSchema.create({
 
 
 const items = {product,price,brand,remainingStock} 
-const catchKey = `stock:${brand}:${productInfo.productType}:${productInfo.flavor}`;
+const catchKey = `stock:${brand}`
 
 await client.set(catchKey, JSON.stringify(items), {
             EX: 3600 // Cache for 1 hour
         });
-console.log("catchkey is ",catchKey);
+console.log("catchKey is ",catchKey);
 
 
 res.status(201).json({message:"product created successfully and successfully catched",data:items})
@@ -72,6 +72,7 @@ return res.status(404).json({"message":"name does not exist"})
 
 }
 
+
     await stockSchema.findOneAndUpdate(
         {brand:brand},
 {
@@ -82,6 +83,11 @@ return res.status(404).json({"message":"name does not exist"})
     }},
     
 )
+const catchKey = `stock:${brand}`
+await client.del(catchKey)
+console.log(catchKey,"is deleted after updating resource");
+
+
 res.status(200).json({"message":"successfully updated resoources"})
 
 } catch (error) {
@@ -132,15 +138,20 @@ const delstock = async (req,res)=>{
 
     try {
 
-
-    const { _id } = req.params;
+        
+    const { _id,brand } = req.params;
 
             if (!_id) {
                 return res.status(400).json({ "message": "Product ID is required" });
             }
 
             // silent del: delete the whole document with req.body
-            await stockSchema.findOneAndDelete(_id)
+           const delProduct =  await stockSchema.findOneAndDelete(_id)
+            const delbrand = brand||delProduct.brand
+            const clientKey = `stock:${delbrand}`
+            await client.del(clientKey)
+            console.log("client key and product is deletd");
+            
             res.status(200).json({"message":"successfully deleted"})
     } catch (error) {
         
@@ -176,7 +187,7 @@ try {
             csvHeader = `_id,brand,price,remainign stocks,\n`
             csvRows = findStock.map(i=>{
 
-                return `${i._id},${i.brand},${i.price},${i.remainingStock}`
+                return `${i._id},${i.brand},${i.pricnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnne},${i.remainingStock}`
 
             }).join('\n')
 
@@ -201,5 +212,47 @@ try {
 
 }
 
+const dynaGet = async (req,res)=>{
 
-module.exports = {addStock,upadateStock,putStock,delstock,staticGet}
+
+try {
+    const { brand } = req.params;
+    if (!brand) {
+        return res.status(400).json({ message: "Brand parameter is required" });
+    }
+    const catchKey = `stock:${brand}`;
+    // Try to get from Redis
+    const catchData = await client.get(catchKey);
+    if (catchData) {
+
+        return res.status(200).json({
+            message: "data is fetched from redis",
+            data: JSON.parse(catchData)
+        });
+    }
+    // If not in cache, fetch from DB
+    const findStock = await stockSchema.findOne({ brand });
+    if (!findStock) {
+        return res.status(404).json({ message: "product not found" });
+    }
+    // Cache the same structure as addStock
+    const items = {
+        product: findStock.product,
+        price: findStock.price,
+        brand: findStock.brand,
+        remainingStock: findStock.remainingStock
+    };
+    await client.set(catchKey, JSON.stringify(items), { EX: 3600 });
+    return res.status(200).json({
+        message: "the data is found from db",
+        data: items
+    });
+} catch (error) {
+    console.error("DynaGet Error:", error);
+    res.status(500).json({ message: "server error", error: error.message });
+}
+}
+
+
+
+module.exports = {addStock,upadateStock,putStock,delstock,staticGet,dynaGet}
